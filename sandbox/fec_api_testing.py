0026cce89db6c1c1a7de99daba9a226c65da7cef
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-01-23 16:59:06 -0600

    Update fec_api_testing.py

--- sandbox/fec_api_testing.py
+++ sandbox/fec_api_testing.py
@@ -47,7 +47,8 @@ def schedule_a(**params): #Schedule A records describe itemized receipts, includ
     new_index = first_res["pagination"]["last_indexes"]
     last_index["last_contribution_receipt_date"] = new_index.get("last_contribution_receipt_date", None)
     last_index["last_index"] = new_index["last_index"]
-    while last_index["last_contribution_receipt_date"] is not None:
+    number = 0
+    while last_index["last_contribution_receipt_date"] is not None and number <= 500: #FIX ME: You have a limited amount of API calls (1000 calls per hour), figure out a work around.
         rec_url = f"https://api.open.fec.gov/v1/schedules/schedule_a/?last_contribution_receipt_date={new_index['last_contribution_receipt_date']}&two_year_transaction_period=2024&per_page=100&last_index={new_index['last_index']}&sort=-contribution_receipt_date&sort_hide_null=false&sort_null_only=false&api_key={params['api_key']}"
         page_req = requests.get(rec_url)
         page_res = page_req.json()
@@ -55,7 +56,10 @@ def schedule_a(**params): #Schedule A records describe itemized receipts, includ
         last_index["last_contribution_receipt_date"] = page_index.get("last_contribution_receipt_date", None)
         last_index["last_index"] = page_index["last_index"]
         result.extend(page_res["results"])
+        number += 1
     print(len(result))
+
+# schedule_a(**fec_params)
 
 
 

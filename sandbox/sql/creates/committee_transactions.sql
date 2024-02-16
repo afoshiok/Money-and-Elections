3e2bc09fe33bd7807515a2228328f61dfbc418f8
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-02-15 21:41:12 -0600

    added create scripts

---
+++ sandbox/sql/creates/committee_transactions.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS committee_transactions (
+    sub_id varchar(9),
+    cmte_id varchar(9),
+    amndt_id varchar(1),
+    rpt_tp varchar(3),
+    transaction_pgi varchar(5),
+    image_num varchar(18),
+    transaction_tp varchar(3),
+    entity_tp varchar(3),
+    name varchar(200),
+    city varchar(30),
+    state varchar(2),
+    zip_code varchar(9),
+    occupation varchar(38),
+    transaction_dt date,
+    transaction_amt number(14,2),
+    other_id varchar(9),
+    tran_id varchar(32),
+    file_num number(22),
+    memo_cd varchar(1),
+    memo_text varchar(100)
+)
---
+++ sandbox/sql/creates/political_parties.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS political_parties(
+    party_code varchar(3),
+    party_code_desc varchar(200),
+    notes varchar(90) 
+)

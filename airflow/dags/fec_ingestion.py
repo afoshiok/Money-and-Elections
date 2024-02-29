75b2da063ef025ce6242e7fe6f8bdcefff375876
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-02-29 14:56:19 -0600

    First two steps of Committee Ingestion PASSED

--- airflow/dags/fec_ingestion.py
+++ airflow/dags/fec_ingestion.py
@@ -1,6 +1,11 @@
 from airflow.decorators import dag, task
-from pendulum import datetime, duration 
 from airflow.operators.empty import EmptyOperator
+
+import pendulum
+from pendulum import datetime, duration 
+import requests
+import os
+import pandas as pd
 
 default_args = {
     "owner": "admin",
@@ -11,18 +16,59 @@ default_args = {
     "retry_delay": duration(minutes=1),
 }
 
+
+
 @dag(
-    start_date=datetime(2024, 1, 1), schedule="@daily"
+    start_date=datetime(2024, 1, 1), schedule="@once", default_args=default_args
 )
-def generate_dag():
+def committee_ingestion():
+    run_date = pendulum.now().to_date_string()
+    zip_path = "./file_store/zipped/"
+    unzipped_path = "./file_store/unzipped/"
+
     @task #Example of TaskFlow use, here's the docs: https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/taskflow.html
-    def empty_opt1():
-        EmptyOperator()
+    def begin():
+        EmptyOperator(task_id="begin")
+    @task
+    def download_header_file():
+        if not os.path.exists(zip_path):
+            os.makedirs(zip_path)
+
+        if not os.path.exists(unzipped_path):
+            os.makedirs(unzipped_path)
+
+        
+        committees_header_url = "https://www.fec.gov/files/bulk-downloads/data_dictionaries/cm_header_file.csv"
+        header_req = requests.get(committees_header_url)
+
+        header_dir = unzipped_path + f"{run_date}_committees_headers.csv"
+
+        with open(header_dir, "wb") as committee_header_file:
+            committee_header_file.write(header_req.content)
+        
+        
+    @task        
+    def download_zipped_file():
+        committees_url = "https://www.fec.gov/files/bulk-downloads/2024/cm24.zip"
+        committees_req = requests.get(committees_url)
+
+        committees_dir = zip_path + f"{run_date}_cm24.zip"
+        with open(committees_dir, "wb") as committees_file:
+            committees_file.write(committees_req.content)
+        
+    
+    @task
+    def clean_up():
+        os.remove(zip_path + f"{run_date}_cm24.zip")
+        os.remove(unzipped_path + f"{run_date}_committees_headers.csv")
+        os.removedirs(zip_path)
+        os.removedirs(unzipped_path)
+
 
     @task
-    def empty_opt2():
-        EmptyOperator()
+    def end():
+        EmptyOperator(task_id="end")
 
-    empty_opt1() >> empty_opt2()
+    begin() >> [ download_header_file() , download_zipped_file() ] >> clean_up() >> end()
 
-generate_dag()
+committee_ingestion()

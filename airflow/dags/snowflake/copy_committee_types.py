7f99fd32385dd6e0fec42aa91c8fa35ae9ca842e
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-03-31 16:17:07 -0500

    LOAD Committee Types table

---
+++ airflow/dags/snowflake/copy_committee_types.py
@@ -0,0 +1,61 @@
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from pendulum import datetime, duration 
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+
+default_args = {
+    "owner": "admin",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": duration(minutes=1),
+}
+
+truncate_committee_types = """
+USE ELECTION.RAW;
+
+TRUNCATE TABLE election.raw.SRC_COMMITTEE_TYPES;
+"""
+
+copy_committee_types = """
+USE ELECTION.PUBLIC;
+
+COPY INTO election.raw.SRC_COMMITTEE_TYPES
+FROM @COMMITTEE_TYPES_STAGE
+ON_ERROR = 'SKIP_FILE_5%'
+FILE_FORMAT = FEC_CSV;
+"""
+
+@dag(
+    start_date=datetime(2024, 1, 1), schedule=None, default_args=default_args
+)
+def copy_committee_types_table():
+    @task
+    def begin():
+        EmptyOperator(task_id="begin")
+
+    @task
+    def truncate_table():
+        snowflake_query = SnowflakeOperator(
+            task_id="snowflake_query",
+            sql=truncate_committee_types,
+            snowflake_conn_id="snowflake_conn"
+        )
+        snowflake_query.execute(context={})
+
+    @task
+    def copy_table():
+        snowflake_query = SnowflakeOperator(
+            task_id="snowflake_query",
+            sql=copy_committee_types,
+            snowflake_conn_id="snowflake_conn"
+        )
+        snowflake_query.execute(context={})
+    
+    @task
+    def end():
+        EmptyOperator(task_id="end")
+
+    begin() >> truncate_table() >> copy_table() >> end()
+copy_committee_types_table()
---
+++ sandbox/sql/creates/tables/committee_types.sql
@@ -0,0 +1,5 @@
+CREATE TABLE src_committee_types (
+CMTE_TYP_CODE varchar(1),
+CMTE_TYP varchar,
+EXPLANATION varchar
+);

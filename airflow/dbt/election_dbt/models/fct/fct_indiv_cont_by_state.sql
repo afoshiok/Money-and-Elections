ef2b89a9bf8fd9eb30c141b8011ed9c9aa950150
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-04-04 17:46:01 -0500

    Update fct_indiv_cont_by_state.sql

--- airflow/dbt/election_dbt/models/fct/fct_indiv_cont_by_state.sql
+++ airflow/dbt/election_dbt/models/fct/fct_indiv_cont_by_state.sql
@@ -1,3 +1,9 @@
 with indiv_cont_source as (
     SELECT * FROM {{ ref('src_indiv_contributions') }}
 )
+
+SELECT
+    state,
+    SUM(transaction_amt) AS sum_individual_cont
+FROM indiv_cont_source
+GROUP BY state

1249c322c88fd90a5d4a72a1a0c6e85fb5615213
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-04-11 12:25:02 -0500

    Update fct_candidate_indiv_cont.sql

--- airflow/dbt/election_dbt/models/fct/fct_candidate_indiv_cont.sql
+++ airflow/dbt/election_dbt/models/fct/fct_candidate_indiv_cont.sql
@@ -16,7 +16,12 @@ SELECT
     cs.cand_name,
     cs.party_code_desc,
     cs.cand_office_st,
-    cs.cand_office,
+    CASE
+        WHEN cs.cand_office = 'H' THEN 'House'
+        WHEN cs.cand_office = 'S' THEN 'Senate'
+        WHEN cs.cand_office = 'P' THEN 'President'
+        ELSE cs.cand_office
+    END as cand_office,
     cs.cand_office_district,
     SUM(ics.transaction_amt) as total_contributions
 FROM cand_source cs
